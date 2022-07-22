e9ab61ac476bdc8bd653c751637d187de09f3749
husfuu <muhammadhusninurfadillah@gmail.com>
2022-07-22 23:15:19 +0800

    add some configuration on tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: '16px'
+    },
+    extend: {
+      colors: {
+        primary: '#87AAAA',
+        main: '#F6D7A7',
+        dark: '#0f172a'
+      },
+      screens: {
+        '2xl': '1320px'
+      }
+    },
   },
   plugins: [],
 }
