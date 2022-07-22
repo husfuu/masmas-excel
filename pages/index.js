c9110eef34b4173edd005d8ea78e645938f11ce0
husfuu <muhammadhusninurfadillah@gmail.com>
2022-07-22 23:16:25 +0800

    add Hero component into index file

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import Hero from "../components/Hero/"
+
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <Hero />
+
+      
+
+    </>
   )
 }
